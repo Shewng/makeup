170d6a652555e9126add46c33eeec0e7c74fdd1c
Shewng <shwong@Shwongs-Mac.local>
2020-10-21 19:12:59 -0600

    initial commit

---
+++ makeup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  makeup
+//
+//  Created by Shwong on 2020-10-21.
+//  Copyright © 2020 Shwong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, worldssasdasdasdasdasd!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
