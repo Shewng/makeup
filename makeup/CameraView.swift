7a4002908475372812459d0c4ca0fba85daee7c9
Shewng <shwong@Shwongs-Mac.local>
2020-10-21 21:09:50 -0600

    upload button works

--- makeup/CameraView.swift
+++ makeup/CameraView.swift
@@ -9,27 +9,100 @@
 import SwiftUI
 
 struct CameraView: View {
+    
+    @State var isShowingImagePicker = false
+    
+    @State var bareFaceImage = UIImage()
+    
     var body: some View {
-        HStack(spacing: 70){
-            Button(action: {
-                print("Camera Was Tapped")
-            }) {
-                Image(systemName: "plus.circle")
-                    .font(.system(size: 70.0))
-                    .foregroundColor(.gray)
-                    .offset(y: -35)
-            }
+        VStack() {
             
-            Button(action: {
-                print("Upload Was Tapped")
-            }) {
-                Image(systemName: "camera.circle")
-                    .font(.system(size: 70.0))
-                    .foregroundColor(.gray)
+            Image(uiImage: bareFaceImage)
+            .resizable()
+            .scaledToFill()
+                .frame(width:250, height: 250)
+                .border(Color.black, width: 1)
+                .clipped()
+            
+                HStack(spacing: 40) {
                     
+                    Button(action: {
+                        self.isShowingImagePicker.toggle()
+                        print("Upload was tapped")
+                        
+                    }) {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 40.0))
+                            .foregroundColor(.gray)
+                    }
+                        
+                    .sheet(isPresented: $isShowingImagePicker, content: {
+                        ImagePickerView(isPresented: self.$isShowingImagePicker, selectedImage: self.$bareFaceImage)
+                    })
+                    
+                    Button(action: {
+                        print("Camera Was Tapped")
+                    }) {
+                        Image(systemName: "camera.circle")
+                            .font(.system(size: 40.0))
+                            .foregroundColor(.gray)
+                        
+                    }
+            }
+        }
+    }
+}
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    
+    @Binding var isPresented: Bool
+    @Binding var selectedImage: UIImage
+    
+    func makeUIViewController(context:
+        UIViewControllerRepresentableContext<ImagePickerView>) ->
+        UIViewController {
+        
+        let controller = UIImagePickerController()
+        controller.delegate = context.coordinator
+        return controller
+    }
+    
+    func makeCoordinator() -> ImagePickerView.Coordinator {
+        return Coordinator(parent: self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        let parent: ImagePickerView
+        init(parent: ImagePickerView) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let selectedImageFromPicker = info[.originalImage] as? UIImage {
+                print(selectedImageFromPicker)
+                self.parent.selectedImage = selectedImageFromPicker
             }
             
+            self.parent.isPresented = false
         }
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: ImagePickerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ImagePickerView>) {
+    }
+}
+
+struct DummyView: UIViewRepresentable {
+    
+    func makeUIView(context: UIViewRepresentableContext<DummyView>) -> UIButton {
+        let button = UIButton()
+        button.setTitle("DUMMY", for: .normal)
+        button.backgroundColor = .white
+        return button
+    }
+    
+    func updateUIView(_ uiView: DummyView.UIViewType, context: UIViewRepresentableContext<DummyView>) {
     }
 }
 
@@ -38,3 +111,5 @@ struct CameraView_Previews: PreviewProvider {
         CameraView()
     }
 }
+
+
