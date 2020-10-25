cc4cc16baa0fdcfcfcc94359a91e29b0ba0f53e7
ucwillzhou <44952147+ucwillzhou@users.noreply.github.com>
2020-10-24 22:59:45 -0600

    Added swipe

--- makeup/CameraView.swift
+++ makeup/CameraView.swift
@@ -8,56 +8,121 @@
 
 import SwiftUI
 
+
+var steps: [String] = ["Picture", "Video"]
+var frameIndex = 0;
+
+//helper to print for debugging
+//https://stackoverflow.com/questions/56517813/how-to-print-to-xcode-console-in-swiftui
+extension View {
+    func Print(_ vars: Any...) -> some View {
+        for v in vars { print(v) }
+        return EmptyView()
+    }
+}
+
 struct CameraView: View {
     
+    
+    @ObservedObject var model = Model()
     @State private var isShowingImagePicker = false
     @State private var showCamera = false
-    @State private var condition = 1
-
+    @State var condition = 1
+    
     @State private var bareFaceImage = UIImage()
-
+    
+    var colors: [Color] = [.blue, .green, .red, .orange]
+    
+    func addFrame() {
+        let id = model.frames.count + 1
+        let image = UIImage()
+        model.frames.append(Frame(id: id, name: "Frame\(id)", image: image))
+    }
+       
+    
     
     var body: some View {
+        
         VStack() {
+            /*
+             HStack(alignment: .center, spacing: 30) {
+             ForEach(steps.indices) { x in
+             Image(uiImage: self.bareFaceImage)
+             .resizable()
+             .scaledToFill()
+             .frame(width:270, height: 300)
+             .border(Color.black, width: 1)
+             .clipped()
+             .padding()
+             }
+             */
             
-            Image(uiImage: bareFaceImage)
-            .resizable()
-            .scaledToFill()
-                .frame(width:250, height: 250)
-                .border(Color.black, width: 1)
-                .clipped()
-                .padding()
+            HStack(alignment: .center, spacing: 30) {
+                
+                Print(type(of: model.frames))
             
-                HStack(spacing: 40) {
-                    
-                    Button(action: {
-                        self.isShowingImagePicker.toggle()
-                        self.condition = 1
-                        print("Upload was tapped")
-                        
-                    }) {
-                        Image(systemName: "plus.circle")
-                            .font(.system(size: 40.0))
-                            .foregroundColor(.gray)
-                    }
-                        
-                    .sheet(isPresented: $isShowingImagePicker, content: {
-                        ImagePickerView(isPresented: self.$isShowingImagePicker, selectedImage: self.$bareFaceImage, flag: self.$condition)
-                    })
-                    
-                    Button(action: {
-                        print("Camera Was Tapped")
-                        self.condition = 2
-                        self.showCamera.toggle()
-                    }) {
-                        Image(systemName: "camera.circle")
-                            .font(.system(size: 40.0))
-                            .foregroundColor(.gray)
-                    }
-                    .sheet(isPresented: $showCamera, content: {
-                        ImagePickerView(isPresented: self.$showCamera, selectedImage: self.$bareFaceImage, flag: self.$condition)
-                    })
+                ForEach(model.frames, id: \.self) { x in
+                    Image(uiImage: x.image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width:270, height: 300)
+                        .border(Color.black, width: 1)
+                        .clipped()
+                        .padding();
 
+                }
+                self.Print(model.frames[0].name)
+                
+                
+                
+            }.modifier(ScrollingHStackModifier(items: model.frames.count, itemWidth: 270, itemSpacing: 60))
+            
+            
+            HStack(spacing: 40) {
+                
+                
+                
+                Button(action: {
+                    self.isShowingImagePicker.toggle()
+                    self.condition = 1
+                    print("Upload was tapped")
+                    
+                }) {
+                    Image(systemName: "plus.circle")
+                        .font(.system(size: 40.0))
+                        .foregroundColor(.gray)
+                }
+                
+                //need to add a index to see which photo to upload to
+                .sheet(isPresented: $isShowingImagePicker, content: {
+                    ImagePickerView(isPresented: self.$isShowingImagePicker, selectedImage: self.$model.frames[frameIndex].image, flag: self.$condition)
+                })
+                
+                Button(action: {
+                    print("Camera Was Tapped")
+                    self.condition = 2
+                    self.showCamera.toggle()
+                }) {
+                    Image(systemName: "camera.circle")
+                        .font(.system(size: 40.0))
+                        .foregroundColor(.gray)
+                }
+                .sheet(isPresented: $showCamera, content: {
+                    ImagePickerView(isPresented: self.$showCamera, selectedImage: self.$bareFaceImage, flag: self.$condition)
+                })
+                
+                Button(action: {
+                    print("Add was tapped")
+                    self.addFrame()
+                    
+                    
+                }) {
+                    Image(systemName: "pencil.tip.crop.circle.badge.plus")
+                        .font(.system(size: 40.0))
+                        .foregroundColor(.gray)
+                }
+                
+                
             }
         }
     }
@@ -77,9 +142,9 @@ struct ImagePickerView: UIViewControllerRepresentable {
     func makeUIViewController(context:
         UIViewControllerRepresentableContext<ImagePickerView>) ->
         UIViewController {
-        
             
-        let controller = UIImagePickerController()
+            
+            let controller = UIImagePickerController()
             if(flag == 1){
                 controller.sourceType = sourceType1
             }
@@ -87,8 +152,8 @@ struct ImagePickerView: UIViewControllerRepresentable {
                 controller.sourceType = sourceType2
                 
             }
-        controller.delegate = context.coordinator
-        return controller
+            controller.delegate = context.coordinator
+            return controller
     }
     
     func makeCoordinator() -> ImagePickerView.Coordinator {
@@ -137,3 +202,28 @@ struct CameraView_Previews: PreviewProvider {
 }
 
 
+class Frame: NSObject {
+    var id: Int
+    var name: String
+    var image: UIImage
+    
+    init(id: Int, name: String, image: UIImage) {
+        self.id = id
+        self.name = name
+        self.image = image
+    }
+}
+
+class Model: ObservableObject {
+    @Published var frames: [Frame] = []
+    @State var b1 = UIImage()
+    @State var b2 = UIImage()
+    
+    init() {
+        frames = [
+            Frame(id: 1, name: "Frame1", image: b1),
+            Frame(id: 2, name: "Frame2", image: b2),
+        ]
+    }
+    
+}
---
+++ makeup/ScrollingStackModifier.swift
@@ -0,0 +1,110 @@
+//
+//  ScrollingStackModifier.swift
+//  makeup
+//
+//  Created by William Zhou on 2020-10-24.
+//  Copyright © 2020 Shwong. All rights reserved.
+//
+
+
+
+//  ScrollingStackModifier.swift
+//  ScrollView_Tests
+//
+//  Created by Jean-Marc Boullianne on 8/7/20.
+//
+import SwiftUI
+import Foundation
+
+
+struct ScrollingHStackModifier: ViewModifier {
+    
+    @State private var scrollOffset: CGFloat
+    @State private var dragOffset: CGFloat
+    
+    var items: Int
+    var itemWidth: CGFloat
+    var itemSpacing: CGFloat
+    init(items: Int, itemWidth: CGFloat, itemSpacing: CGFloat) {
+        self.items = items
+        self.itemWidth = itemWidth
+        self.itemSpacing = itemSpacing
+        
+        // Calculate Total Content Width
+        let contentWidth: CGFloat = CGFloat(items) * itemWidth + CGFloat(items - 1) * itemSpacing
+        let screenWidth = UIScreen.main.bounds.width
+        
+        // Set Initial Offset to first Item
+        let initialOffset = (contentWidth/2.0) - (screenWidth/2.0) + ((screenWidth - itemWidth) / 2.0)
+        
+        self._scrollOffset = State(initialValue: initialOffset)
+        self._dragOffset = State(initialValue: 0)
+        
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .offset(x: scrollOffset + dragOffset, y: 0)
+            .gesture(DragGesture()
+                .onChanged({ event in
+                    self.dragOffset = event.translation.width
+                })
+                .onEnded({ event in
+                    // Scroll to where user dragged
+                    self.scrollOffset += event.translation.width
+                    self.dragOffset = 0
+                    
+                    // Now calculate which item to snap to
+                    let contentWidth: CGFloat = CGFloat(self.items) * self.itemWidth + CGFloat(self.items - 1) * self.itemSpacing
+                    let screenWidth = UIScreen.main.bounds.width
+                    
+                    // Center position of current offset
+                    let center = self.scrollOffset + (screenWidth / 2.0) + (contentWidth / 2.0)
+                    
+                    // Calculate which item we are closest to using the defined size
+                    var index = (center - (screenWidth / 2.0)) / (self.itemWidth + self.itemSpacing)
+                    
+                    
+                    // Should we stay at current index or are we closer to the next item...
+                    if index.remainder(dividingBy: 1) > 0.5 {
+                        index += 1
+                        print("If Swiped to", index)
+                        
+                    } else {
+                        index = CGFloat(Int(index))
+                        frameIndex = abs(Int(index) - 1)
+                        
+                        
+                        
+                        
+                        print(frameIndex)
+                        if(index == 0){
+                            //steps.append("Video")
+                            
+                            
+                        }
+                        //print("Steps", steps)
+                    }
+                    
+                    // Protect from scrolling out of bounds
+                    index = min(index, CGFloat(self.items) - 1)
+                    index = max(index, 0)
+                    
+                    // Set final offset (snapping to item)
+                    let newOffset = index * self.itemWidth + (index - 1) * self.itemSpacing - (contentWidth / 2.0) + (screenWidth / 2.0) - ((screenWidth - self.itemWidth) / 2.0) + self.itemSpacing
+                    
+                    // Animate snapping
+                    withAnimation {
+                        self.scrollOffset = newOffset
+                    }
+                    
+                })
+            )
+    }
+}
+
+struct ScrollingStackModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
